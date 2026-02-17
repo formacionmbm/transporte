4af2f978ad7980898e87372877c30a9b2362c950
yulikof <kobra4beta@gmail.com>
2026-02-17 11:07:47 +0100

    se implemento clase Parada

--- src/main/java/com/practica/transporte/entities/Parada.java
+++ src/main/java/com/practica/transporte/entities/Parada.java
@@ -1,4 +1,20 @@
 package com.practica.transporte.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
 public class Parada {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private int numeroParada;
+    private String nombre;
+
+
 }
