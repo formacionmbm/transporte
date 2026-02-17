31166be66af73444711a82d22e1b08de2d80648d
MDVeinte <maurodesruelles@gmail.com>
2026-02-17 11:24:38 +0100

    EntityConductor

--- src/main/java/com/practica/transporte/entities/Conductor.java
+++ src/main/java/com/practica/transporte/entities/Conductor.java
@@ -1,4 +1,31 @@
 package com.practica.transporte.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Conductor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "num_empleado", unique = true, nullable = false)
+    private String numEmpleado;
+
+    @Column(name="tipo_licencia")
+    private String tipoLicencia;
+
+    @Column(name="caducidad_licencia")
+    private LocalDate caducidadLicencia;
+
+    @Column(name="disponible")
+    private boolean disponible;
+
 }
