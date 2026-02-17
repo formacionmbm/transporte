5e3d40254fc6ec5beb17105a82ac9993f189ab43
kisho-stack <jorgesc1903@gmail.com>
2026-02-17 11:21:16 +0100

    se implemento ParadaRepository

--- src/main/java/com/practica/transporte/repositories/ParadaRepository.java
+++ src/main/java/com/practica/transporte/repositories/ParadaRepository.java
@@ -1,4 +1,12 @@
 package com.practica.transporte.repositories;
 
-public interface ParadaRepository {
+import com.practica.transporte.entities.Parada;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ParadaRepository extends JpaRepository<Parada,Long> {
+
+    Optional<Parada> findByNumeroParada(int numeroParada);
+
 }
