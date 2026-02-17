dd485074b773b29035f92a2ac516f150437ecb73
MDVeinte <maurodesruelles@gmail.com>
2026-02-17 13:09:07 +0100

    Corregido ConductorRepository 2

--- src/main/java/com/practica/transporte/repositories/ConductorRepository.java
+++ src/main/java/com/practica/transporte/repositories/ConductorRepository.java
@@ -2,10 +2,11 @@ package com.practica.transporte.repositories;
 
 import com.practica.transporte.entities.Conductor;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface ConductorRepository extends JpaRepository<Conductor, Long> {
     Optional<Conductor> findByNumeroEmpleado(String numeroEmpleado);
 }
