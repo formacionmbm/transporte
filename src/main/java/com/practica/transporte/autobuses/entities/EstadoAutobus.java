1e8d575c22e38765f0be660ca46721009972f7a6
Mananas <fran@>
2026-02-17 13:45:08 +0100

    .

--- src/main/java/com/practica/transporte/autobuses/entities/EstadoAutobus.java
+++ src/main/java/com/practica/transporte/autobuses/entities/EstadoAutobus.java
@@ -1,4 +1,15 @@
 package com.practica.transporte.autobuses.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="ESTADOS_AUTOBUS")
 public class EstadoAutobus {
+
+    @Id
+    private Long id;
+
+    private String nombre;
 }
