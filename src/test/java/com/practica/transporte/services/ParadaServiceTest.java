ff48a10906b269ea820c5917eec44eac39b5605a
yulikof <kobra4beta@gmail.com>
2026-02-18 09:56:55 +0100

    se termina de implementar el paradaServiceTest

--- src/test/java/com/practica/transporte/services/ParadaServiceTest.java
+++ src/test/java/com/practica/transporte/services/ParadaServiceTest.java
@@ -5,10 +5,18 @@ import com.practica.transporte.repositories.ParadaRepository;
 import com.practica.transporte.services.exception.ServiceException;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
 
 @Slf4j
+@ExtendWith(MockitoExtension.class)
 public class ParadaServiceTest {
 
     @Mock
@@ -18,13 +26,22 @@ public class ParadaServiceTest {
     private ParadaService service;
 
     @Test
-    void buscarPorNumero_ok() throws ServiceException{
-        log.info("[buscarPorNumero]");
+    void buscarPorNumeroPararda_ok() throws ServiceException{
+        log.info("[buscarPorNumeroPararda_ok]");
 
         //given
         Parada paradatest = new Parada();
         paradatest.setNumeroParada(101);
         paradatest.setNombre("oporto");
+    }
+
+    @Test
+    void buscarPorNumeroParada_ko() {
+        // Given
+        when(repository.findByNumeroParada(999)).thenReturn(Optional.empty());
+
+        // When & Then (Súper corto)
+        assertThrows(ServiceException.class, () -> service.buscarPorNumero(999));
     }
 
 }
